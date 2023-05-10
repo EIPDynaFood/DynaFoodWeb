391a74713b5024aea740d9fc60eae1a731c55ff2
O Elman <oelman@localhost.localdomain>
2023-05-10 12:35:04 +0900

    Updates

---
+++ precache-manifest.0b19668f74d6b7d0a4bb61de95ba6d1c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2139e47c5e3e61fa752388d7a56f45d",
+    "url": "/DynaFoodWeb/index.html"
+  },
+  {
+    "revision": "9db1565085a741d42ea9",
+    "url": "/DynaFoodWeb/static/css/2.ee96fe6c.chunk.css"
+  },
+  {
+    "revision": "cbd090d74314c70e3fcb",
+    "url": "/DynaFoodWeb/static/css/main.e612eb19.chunk.css"
+  },
+  {
+    "revision": "9db1565085a741d42ea9",
+    "url": "/DynaFoodWeb/static/js/2.0d572f3b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/DynaFoodWeb/static/js/2.0d572f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd090d74314c70e3fcb",
+    "url": "/DynaFoodWeb/static/js/main.c965e4ff.chunk.js"
+  },
+  {
+    "revision": "fde1ef35a2ffc07eb668",
+    "url": "/DynaFoodWeb/static/js/runtime-main.c4f5c23a.js"
+  },
+  {
+    "revision": "746b33c20d0fa766f91eee76dc754faa",
+    "url": "/DynaFoodWeb/static/media/information.746b33c2.png"
+  },
+  {
+    "revision": "3a78a1b567b5ac8226f4341abe5fa8e4",
+    "url": "/DynaFoodWeb/static/media/logo_frame.3a78a1b5.png"
+  },
+  {
+    "revision": "79a33c601c186e6b84681237cd7ade4d",
+    "url": "/DynaFoodWeb/static/media/logo_frame_invisible.79a33c60.png"
+  },
+  {
+    "revision": "d0ceb455082e55f4a2fa283e595ae504",
+    "url": "/DynaFoodWeb/static/media/phase1.d0ceb455.png"
+  },
+  {
+    "revision": "4cd497395dc78e453d841d99bc6626c9",
+    "url": "/DynaFoodWeb/static/media/phase2.4cd49739.png"
+  },
+  {
+    "revision": "30bb944b112eef0a08ae3b2e44b97081",
+    "url": "/DynaFoodWeb/static/media/phase3.30bb944b.png"
+  },
+  {
+    "revision": "cae41cb476c69451a2b915d674380f72",
+    "url": "/DynaFoodWeb/static/media/scanner.cae41cb4.png"
+  },
+  {
+    "revision": "131ec9f06e5ded66921fc619fa4a4d91",
+    "url": "/DynaFoodWeb/static/media/video-4.131ec9f0.mp4"
+  }
+]);
