94ae61c783c6c9b4f771f05c28a57a38da96b534
Lord-bledard <pol-antoine.loiseau@epitech.eu>
2023-09-21 18:31:31 +0200

    [Edit] : Profil, First Link To Back

--- src/components/pages/Profil.js
+++ src/components/pages/Profil.js
@@ -1,42 +1,149 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../Footer';
 import { useHistory } from 'react-router-dom';
 import videoFile from '../../images/video-4.mp4';
 import axios from 'axios';
 
+
 // Utilisez un état local pour suivre la couleur de fond actuelle des boutons
 function Profil() {
   // Utilisez un état local pour suivre la couleur de fond actuelle des boutons
-  const [veganButtonColor, setVeganButtonColor] = useState("lightgreen");
-  const [vegetarienButtonColor, setVegetarienButtonColor] = useState("lightgreen");
+  const [veganButtonColor, setVeganButtonColor] = useState();
+  const [vegetarienButtonColor, setVegetarienButtonColor] = useState();
   const history = useHistory();
   // Utilisez un état local pour suivre le texte actuel des boutons
-  const [veganButtonText, setVeganButtonText] = useState("No");
-  const [vegetarienButtonText, setVegetarienButtonText] = useState("No");
+  const [veganButtonText, setVeganButtonText] = useState();
+  const [vegetarienButtonText, setVegetarienButtonText] = useState();
 
   // Utilisez un état local pour suivre les éléments ajoutés
   const [elementsAjoutes, setElementsAjoutes] = useState([]);
 
   // Fonction pour changer la couleur de fond du bouton et le texte
-  const changeButtonColorAndText = (button, currentColor, text, setText) => {
+  const changeButtonColorAndText = () => {
     const colors = ["lightgreen", "limegreen", "green"];
-    const currentIndex = colors.indexOf(currentColor);
+    const currentIndex = colors.indexOf(veganButtonColor);
     const nextIndex = (currentIndex + 1) % colors.length;
     const nextColor = colors[nextIndex];
-
+    var value = 0
     // Mettre à jour la couleur du bouton
-    button(nextColor);
-
+    setVeganButtonColor(nextColor);
+    console.log(nextColor)
     // Mettre à jour le texte en fonction de la couleur
     if (nextColor === "limegreen") {
-      setText("Casual");
+      setVeganButtonText("Casual");
+      value = 1
     } else if (nextColor === "green") {
-      setText("Strict");
+      setVeganButtonText("Strict");
+      value = 2
     } else {
-      setText("No");
+      setVeganButtonText("No");
+      value = 0
     }
+      var qs = require('qs');
+        var data = qs.stringify({
+            'alertActivation': `${true}`,
+            'restrictionName': 'vegan',
+            'strongness': `${value}`,
+        });
+        console.log(data)
+        var config = {
+            method: 'patch',
+            url: 'http://x2024dynafood545437452001.westeurope.cloudapp.azure.com:8081/' + 'settings',
+            data : data,
+            headers: {
+              Authorization: `Bearer `+ localStorage.getItem("token"),
+            }
+        };
+        axios(config)
+        .then().catch((err) => {
+            if (err.response.status === 401)
+                throw(err)
+        })
+
+  };
+  
+  const changeButtonColorAndText2 = () => {
+    const colors = ["lightgreen", "limegreen", "green"];
+    const currentIndex = colors.indexOf(vegetarienButtonColor);
+    const nextIndex = (currentIndex + 1) % colors.length;
+    const nextColor = colors[nextIndex];
+    var value = 0
+    // Mettre à jour la couleur du bouton
+    setVegetarienButtonColor(nextColor);
+    console.log(currentIndex)
+    // Mettre à jour le texte en fonction de la couleur
+    if (nextColor === "limegreen") {
+      setVegetarienButtonText("Casual");
+      value = 1
+    } else if (nextColor === "green") {
+      setVegetarienButtonText("Strict");
+      value = 2
+    } else {
+      setVegetarienButtonText("No");
+      value = 0
+    }
+      var qs = require('qs');
+        var data = qs.stringify({
+            'alertActivation': `${true}`,
+            'restrictionName': 'vegetarian',
+            'strongness': `${value}`,
+        });
+        var config = {
+            method: 'patch',
+            url: 'http://x2024dynafood545437452001.westeurope.cloudapp.azure.com:8081/' + 'settings',
+            data : data,
+            headers: {
+              Authorization: `Bearer `+ localStorage.getItem("token"),
+            }
+        };
+        axios(config)
+        .then().catch((err) => {
+            if (err.response.status === 401)
+                throw(err)
+        })
   };
 
+
+  useEffect(() => {
+    const config = {
+        method: 'get',
+        url: 'http://x2024dynafood545437452001.westeurope.cloudapp.azure.com:8081/' + 'settings',
+        headers: {
+          Authorization: `Bearer `+ localStorage.getItem("token"),
+        }
+    };
+    axios(config)
+        .then(function (response) {
+          console.log(response.data)
+          const valueVeg = parseInt(response.data[0].strongness)
+          const valueVeg2 = parseInt(response.data[1].strongness)
+          if (valueVeg === 0) {
+            setVeganButtonColor("lightgreen");
+            setVeganButtonText("No")
+          } else if (valueVeg === 1) {
+            setVeganButtonColor("limegreen");
+            setVeganButtonText("Casual")
+          } else if (valueVeg === 2) {
+            setVeganButtonColor("green");
+            setVeganButtonText("Strict")
+          }
+          if (valueVeg2 === 0) {
+            setVegetarienButtonColor("lightgreen");
+            setVegetarienButtonText("No")
+          } else if (valueVeg2 === 1) {
+            setVegetarienButtonColor("limegreen");
+            setVegetarienButtonText("Casual")
+          } else if (valueVeg2 === 2) {
+            setVegetarienButtonColor("green");
+            setVegetarienButtonText("Strict")
+          }
+        })
+        .catch(function (error) {
+            if (error.response.status === 401)
+                throw(error);
+            alert(error);
+        });
+})
   const handleLogout = () => {
     // Effectuez ici toute logique de déconnexion, par exemple, déconnexion depuis votre backend, suppression de jetons, etc.
     localStorage.removeItem('token');
@@ -44,12 +151,59 @@ function Profil() {
     // Actualisez la page pour déconnecter l'utilisateur
     history.push('/');
     window.location.reload();
+    
+  };
+
+  const handleTest = () => {
+    // Effectuez ici toute logique de déconnexion, par exemple, déconnexion depuis votre backend, suppression de jetons, etc.
+    //localStorage.removeItem('token');
+    //localStorage.removeItem('refresh_token');
+    // Actualisez la page pour déconnecter l'utilisateur
+    //history.push('/');
+    //window.location.reload();
+    const config = {
+      method: 'get',
+      url: 'http://x2024dynafood545437452001.westeurope.cloudapp.azure.com:8081/' + 'settings',
+      
+  };
+  axios(config)
+      .then(function (response) {
+          alert(response.data)
+      })
+      .catch(function (error) {
+          alert(error);
+      });
   };
 
   
   // Fonction pour ajouter un élément à la liste
   const ajouterElement = (element) => {
-    setElementsAjoutes([...elementsAjoutes, element]);
+
+    if (!elementsAjoutes.includes(element)) {
+      console.log(element)
+      let qs = require('qs')
+      let data = qs.stringify({
+                'alertActivation': `${true}`,
+                'restrictionName': `${element.toLowerCase()}`,
+                'strongness': `${2}`
+            })
+      const config = {
+        method: 'post',
+        url: 'http://x2024dynafood545437452001.westeurope.cloudapp.azure.com:8081/' + 'settings',
+        data: data,
+        headers: {
+          Authorization: `Bearer `+ localStorage.getItem("token"),
+          }
+        };
+        axios(config).then(function (response) {
+          alert(response.data)
+          setElementsAjoutes([...elementsAjoutes, element])
+      })
+      .catch(function (error) {
+          alert(error);
+      });        
+    }
+    
   };
 
   // Fonction pour supprimer un élément de la liste
@@ -130,10 +284,6 @@ function Profil() {
             style={{ ...buttonStyle, backgroundColor: veganButtonColor }}
             onClick={() =>
               changeButtonColorAndText(
-                setVeganButtonColor,
-                veganButtonColor,
-                veganButtonText,
-                setVeganButtonText
               )
             }
           >
@@ -142,18 +292,14 @@ function Profil() {
           <div>{veganButtonText}</div>
         </div>
         <div>
-          <button
+        <button
             style={{ ...buttonStyle, backgroundColor: vegetarienButtonColor }}
             onClick={() =>
-              changeButtonColorAndText(
-                setVegetarienButtonColor,
-                vegetarienButtonColor,
-                vegetarienButtonText,
-                setVegetarienButtonText
+              changeButtonColorAndText2(
               )
             }
           >
-            Végétarien
+            Vegetarian
           </button>
           <div>{vegetarienButtonText}</div>
         </div>
@@ -178,6 +324,9 @@ function Profil() {
         </div>
         <button onClick={handleLogout} style={logoutButtonStyle}>
           Logout
+        </button>
+        <button onClick={handleTest} style={logoutButtonStyle}>
+          Test
         </button>
       </div>
       <Footer />
