5f434ba873d8f1528aca4d46b6d0ea1284ca7cec
Lord-bledard <pol-antoine.loiseau@epitech.eu>
2024-01-12 05:54:38 +0100

    [Fix] : Download APK

--- src/components/pages/Download.js
+++ src/components/pages/Download.js
@@ -2,20 +2,49 @@ import React, { useState } from 'react';
 import Footer from '../Footer';
 import { Button } from './../Button';
 import './Download.css';
+import axios from 'axios';
 
 const Download = () => {
   const [selectedPlatform, setSelectedPlatform] = useState(null);
 
-  // Function to handle the download based on the selected platform
+  const config = {
+    method: 'get',
+    url: 'http://x2024dynafood545437452001.westeurope.cloudapp.azure.com:8081/download',
+    responseType: 'blob', // Indique à axios de traiter la réponse comme un objet Blob
+    rejectUnauthorized: false,
+  };
+  
+  // Fonction pour gérer le téléchargement en fonction de la plateforme sélectionnée
   const handleDownload = (platform) => {
     if (platform === 'ios') {
-      // iOS version
+      // Version iOS
       const url = 'exp://u.expo.dev/update/e5bd7658-7816-4e9b-b240-602915b282ba';
       window.open(url, '_blank');
     } else if (platform === 'android') {
-      // Android version
-      const url = 'https://we.tl/t-sicyVqHNdI';
-      window.open(url, '_blank');
+      // Version Android
+      axios(config)
+        .then(function (response) {
+          // Créer un objet Blob à partir de la réponse
+          const blob = new Blob([response.data], { type: response.headers['content-type'] });
+  
+          // Créer un objet URL à partir du Blob
+          const url = window.URL.createObjectURL(blob);
+  
+          // Créer un lien de téléchargement
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', 'Dynafood.apk'); // Remplacez 'nom-du-fichier' par le nom souhaité
+          document.body.appendChild(link);
+  
+          // Déclencher le clic sur le lien pour commencer le téléchargement
+          link.click();
+  
+          // Supprimer le lien après le téléchargement
+          document.body.removeChild(link);
+        })
+        .catch((error) => {
+          alert(error.response.data.Error);
+        });
     }
   };
 
